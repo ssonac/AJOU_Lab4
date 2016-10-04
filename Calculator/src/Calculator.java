60b9398596484dfc2f5ebfb32cb5ac4f5d9400f2
문태 <@Vantastic>
2016-10-04 11:49:28 +0900

    hello

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -25,9 +25,9 @@ public class Calculator {
 		}
 	
 	public int remainder (double x, double y){
-		double result=x/y;
-		return (int)result;
-	
+		int result=(int)(x%y);
+		return result;
+		
 		}
 	public String toString (){
 		//return member names, IDs, and repository URI
