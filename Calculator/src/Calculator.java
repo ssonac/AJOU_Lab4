d1be8ca78c0b49dc3a5d39d6ca2317ee0c6ec0ca
Lee Hyemi <hyemi0320@gmail.com>
2016-10-04 12:00:23 +0900

    formatting

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,39 +1,38 @@
 
 public class Calculator {
 
-	
-	public double add (double x, double y){
-		double a = x+y;
+	public double add(double x, double y) {
+		double a = x + y;
 		return a;
-		}
-	
-	public double subract (double x, double y){
-		double result=x-y;
+	}
+
+	public double subract(double x, double y) {
+		double result = x - y;
 		return result;
-		}
-	
-	public double divide (double x, double y){
-		if(y==0)
-		{
+	}
+
+	public double divide(double x, double y) {
+		if (y == 0) {
 			System.out.println("Error: Division is Zero!!");
 			return -1;
 		}
-		double result=x/y;
+		double result = x / y;
 		return result;
-		}
-	
-	public double multiply (double x, double y){
-		double m = x*y;
+	}
+
+	public double multiply(double x, double y) {
+		double m = x * y;
 		return m;
-		}
-	
-	public int remainder (double x, double y){
-		int result=(int)(x%y);
+	}
+
+	public int remainder(double x, double y) {
+		int result = (int) (x % y);
 		return result;
-		}
-	public String toString (){
-		//return member names, IDs, and repository URI
-		String s= "Lee Hangyu, Nam Muntaek, Lee Minju_ssonac, muntaekNam, Minnnn_https://github.com/ssonac/AJOU_Lab4";
+	}
+
+	public String toString() {
+		// return member names, IDs, and repository URI
+		String s = "Lee Hangyu, Nam Muntaek, Lee Minju_ssonac, muntaekNam, Minnnn_https://github.com/ssonac/AJOU_Lab4";
 		return s;
 	}
 }
