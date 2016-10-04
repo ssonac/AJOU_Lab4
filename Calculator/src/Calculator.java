72b196bbb3eeeb48a8832b87a3383912afb74b94
jaehwan <jaehwanbn@gmail.com>
2016-10-04 10:30:06 +0900

    START

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,28 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public double add (double x, double y){
+		return 0;
+		}
+	
+	public double subract (double x, double y){
+		return 0;
+		}
+	
+	public double divide (double x, double y){
+		return 0;
+		}
+	
+	public double multiply (double x, double y){
+		return 0;
+		}
+	
+	public double remainder (double x, double y){
+		return 0;
+		}
+}
