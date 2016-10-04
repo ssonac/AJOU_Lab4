649cabc608593388000a8d46e919c1c77735e40b
문태 <@Vantastic>
2016-10-04 10:59:10 +0900

    dddd

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -3,33 +3,35 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Calculator P = new Calculator();
+
 	}
 	
 	public double add (double x, double y){
-		double a = x+y;
-		return a;
+		return 0;
 		}
 	
 	public double subract (double x, double y){
-		return 0;
+		double result = x-y;
+		return result;
 		}
 	
 	public double divide (double x, double y){
-		return 0;
+		if(y==0)
+		{
+			System.out.println("Error: Division is Zero!!");
+			return -1;
+		}
+		double result=x/y;
+		return result;
 		}
 	
 	public double multiply (double x, double y){
-		double m = x*y;
-		return m;
+		return 0;
 		}
 	
 	public int remainder (double x, double y){
+		//int result=x/y;
+//		return result;
 		return 0;
 		}
-	public String toString (){
-		//return member names, IDs, and repository URI
-		String s= "Lee Hangyu, Nam Muntaek, Lee Minju_ssonac, muntaekNam, Minnnn_https://github.com/ssonac/AJOU_Lab4";
-		return s;
-	}
 }
